09369c39374bc8b5f1b301fa9289162b09058239
Technoask <eithan.uretsky1@gmail.com>
2025-05-28 14:05:31 +0300

    added an implementation for levels

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -6,10 +6,8 @@ using UnityEngine.Rendering;
 
 public class Goal : MonoBehaviour
 {
-    [SerializeField] private GameObject Timer;
     [SerializeField] private GameObject winText;
     [SerializeField] private GameObject NextButton;
-    [SerializeField] private GameObject ReplayButton;
     [SerializeField] private GameObject BlurEffect;
 
 
@@ -26,13 +24,9 @@ public class Goal : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        GameManager.GetComponent<Timer>().enabled = false;
         GameManager.GetComponent<WeatherController>().manageBlur(GameManager.GetComponent<WeatherController>().objectsToBlur, layerToBlur);
-        Timer.SetActive(false);
         winText.SetActive(true);
-        winText.GetComponent<TextMeshProUGUI>().text = "You Win!";
         NextButton.SetActive(true);
-        ReplayButton.SetActive(true);
         volume.enabled = true;
         pControls.DisableInput();
     }
--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -1,15 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public void LoadNextLevel()
+    [SerializeField] private List<GameObject> levels;
+    private int currentLevel = 0;
+    private WeatherController gameManager;
+
+    private void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        gameManager = GetComponent<WeatherController>();
+        enableChildren(levels[currentLevel], true);
     }
 
-    public void ReloadLevel()
+    public void enableChildren(GameObject parent, bool value)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        parent.SetActive(value);
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            parent.transform.GetChild(i).gameObject.SetActive(value);
+        }
     }
+
+    public void setObjectsToBlur()
+    {
+        List<GameObject> list = new List<GameObject>();
+        for (int i = 0;i < levels[currentLevel].transform.childCount; i++)
+        {
+            GameObject child = levels[currentLevel].transform.GetChild(i).gameObject;
+            if (child.layer != LayerMask.GetMask("UI") || child.layer != LayerMask.GetMask("BG Blur")) 
+                list.Add(child);
+        }
+        gameManager.objectsToBlur = list;
+    }
+
+    public void LoadNextLevel()
+    {
+        currentLevel++;
+        enableChildren(levels[currentLevel-1], false);
+        enableChildren(levels[currentLevel], true);
+        setObjectsToBlur();
+    }
+
+
 }
